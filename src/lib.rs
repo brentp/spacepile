bcc9c25b001241fd73dff3368464d469c3439ad4
Brent Pedersen <bpederse@gmail.com>
2022-12-16 04:51:57 -0800

    working!

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@ pub fn iterate(bam_path: String, grouping_tag: String) -> Result<usize> {
     let mut bam = Reader::from_path(&bam_path)?;
 
     let mut sp = SpacePile {
-        bam: bam,
+        bam,
         grouping_tag: String::from(grouping_tag),
         last_group: String::from(";;;;;;;"),
         max_length: 152,
@@ -60,22 +60,13 @@ pub fn iterate(bam_path: String, grouping_tag: String) -> Result<usize> {
     Ok(0)
 }
 
-struct CigTracker<'a> {
-    cigar: &'a CigarStringView,
-    len: u32,
-    op_i: u16,
-    /// tracks which op we are in.
-    off: u16,
-    /// total offset within the read.
-    dels: u16,
-}
-
-impl Index<usize> for CigTracker<'_> {
-    type Output = u32;
-    fn index(&self, i: usize) -> &u32 {
-        // TODO:
-        &0
-    }
+enum Encoding {
+    A = 0,
+    C = 1,
+    G = 2,
+    T = 3,
+    End = -1,
+    Space = -2,
 }
 
 pub fn space(cigars: &Vec<CigarStringView>, max_length: u16) -> Result<Vec<Vec<u16>>> {
@@ -83,10 +74,11 @@ pub fn space(cigars: &Vec<CigarStringView>, max_length: u16) -> Result<Vec<Vec<u
         .iter()
         .map(|c| CigTracker {
             cigar: c,
+            idx: 0,
             dels: 0,
             op_i: 0,
-            off: 0,
-            len: c.iter().map(|o| o.len()).sum(),
+            op_off: 0,
+            len: c.iter().map(|o| o.len() as u16).sum(),
         })
         .collect();
     let max_len = std::cmp::min(
@@ -94,17 +86,85 @@ pub fn space(cigars: &Vec<CigarStringView>, max_length: u16) -> Result<Vec<Vec<u
         offsets.iter().map(|c| c.len).max().unwrap() as u16,
     );
 
-    let result: Vec<Vec<u16>> = offsets
+    let mut result: Vec<Vec<u16>> = offsets
         .iter()
         .map(|_| Vec::with_capacity(max_len as usize))
         .collect();
 
-    while true {
-        // BRENT start here.
-        break;
+    let mut L = 0;
+    while offsets.iter().any(|o| o.idx < o.len as u16) {
+        let any_insertion = offsets.iter().any(|o| {
+            if let Cigar::Ins(_) = o.cigar[o.op_i as usize] {
+                true
+            } else {
+                false
+            }
+        });
+        if any_insertion {
+            // found an insertion so we have to add a space in any read that did not have an
+            // insertion. and add the base for the ones that did.
+            offsets.iter_mut().enumerate().for_each(|(i, o)| {
+                if result[i].len() > L {
+                    // continue
+                } else if o.idx >= o.len {
+                    result[i].push(Encoding::End as u16);
+                } else if let Cigar::Ins(ilen) = o.cigar[o.op_i as usize] {
+                    o.op_off += 1;
+                    if o.op_off == ilen as u16 {
+                        o.op_i += 1;
+                        o.op_off = 0;
+                    }
+                    result[i].push(o.idx - o.dels);
+                    o.idx += 1;
+                } else {
+                    result[i].push(Encoding::Space as u16)
+                }
+            });
+        } else {
+            // no insertion so we add the base or del at each position.
+            offsets.iter_mut().enumerate().for_each(|(i, o)| {
+                if result[i].len() > L {
+                    // continue
+                } else if o.idx >= o.len {
+                    result[i].push(Encoding::End as u16);
+                } else {
+                    if let Cigar::Del(ilen) = o.cigar[o.op_i as usize] {
+                        o.op_off += 1;
+                        if o.op_off == ilen as u16 {
+                            o.op_i += 1;
+                            o.op_off = 0;
+                        }
+                        result[i].push(Encoding::Space as u16);
+                    } else {
+                        result[i].push(o.idx - o.dels);
+                    }
+                    o.idx += 1;
+                }
+            });
+        }
+
+        L += 1;
     }
 
-    Ok(vec![])
+    Ok(result)
+}
+
+impl Index<usize> for CigTracker<'_> {
+    type Output = u32;
+    fn index(&self, i: usize) -> &u32 {
+        &0
+    }
+}
+
+struct CigTracker<'a> {
+    cigar: &'a CigarStringView,
+    len: u16,
+    op_i: u16,
+    /// tracks which op we are in.
+    op_off: u16,
+    idx: u16,
+    /// total offset within the read.
+    dels: u16,
 }
 
 #[cfg(test)]
@@ -121,6 +181,7 @@ mod tests {
         let b = make(vec![Cigar::Match(2), Cigar::Ins(3), Cigar::Match(2)], 0);
         let cigs = vec![a, b];
         eprintln!("size:{:?}", std::mem::size_of::<CigTracker>());
-        space(&cigs, 7).expect("oh no");
+        let s = space(&cigs, 7).expect("oh no");
+        eprintln!("{:?}", s);
     }
 }
